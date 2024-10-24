8a7c7471a0284ab0b750a4e2dee6da50471216f5
sistemassaitempsa <sistemassaitempsa@saitempsa.com>
2024-10-24 13:56:15 -0500

    se agrega el campo observacion 2

--- app/Http/Controllers/limitesCrmController.php
+++ app/Http/Controllers/limitesCrmController.php
@@ -9,7 +9,7 @@ class limitesCrmController extends Controller
     public function getLimitesCrm(){
         $limites = new \stdClass();
     $limites->limite_evidencias = 10;
-    $limites->limite_compromisos = 6;
+    $limites->limite_compromisos = 10;
 
     return response()->json(['status' => 'success', 'message' => 'limites obtenidos correctamente', 'limites' => $limites]);
 
