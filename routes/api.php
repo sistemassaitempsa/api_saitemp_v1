3db09d1268b5a96db74117a19d2061454534d3c8
sistemassaitempsa <sistemassaitempsa@saitempsa.com>
2024-12-31 12:17:19 -0500

    se hace filtro para fecha de ingresos y prioridad

--- routes/api.php
+++ routes/api.php
@@ -839,6 +839,7 @@ Route::group([
   Route::get('/afp', [AfpFormularioIngresoController::class, 'index']);
 
   Route::get('/formularioingreso/{cantidad}', [formularioGestionIngresoController::class, 'index']);
+  Route::get('/formularioIngreso/filtrofechaingreso/{cantidad}', [formularioGestionIngresoController::class, 'filtroFechaIngreso']);
   Route::get('/formularioingresobyid/{id}', [formularioGestionIngresoController::class, 'byid']);
   Route::post('/formularioingreso', [formularioGestionIngresoController::class, 'create']);
   Route::post('/formularioingresopendientes', [formularioGestionIngresoController::class, 'pendientes']);
